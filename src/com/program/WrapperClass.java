0952f3d83885c5e7f39413ddf7541444cd9342d2
Privith Sadhu <131607499+privith-reddy@users.noreply.github.com>
2024-02-20 23:00:33 -0500

    Add files via upload

---
+++ src/com/program/WrapperClass.java
@@ -0,0 +1,36 @@
+package com.program;
+
+public class WrapperClass {
+
+	public static void main(String[] args) 
+	{
+		Integer num1 = 100;
+		Integer num2 = 100;
+		Integer num3 = 500;
+		Integer num4 = 500; 
+		// Wrapper classes having values from -127 to 128
+		
+		
+		if(num1 == num2) 
+		{
+			System.out.println("num1 == num2");
+		}
+		else 
+		{
+			System.out.println("num1 != num2");
+		}
+		
+		if(num3 == num4) 
+		{
+			System.out.println("num3 == num4");
+		}
+		else 
+		{
+			System.out.println("num3 != num4");
+		}
+		 
+		
+
+	}
+
+}
